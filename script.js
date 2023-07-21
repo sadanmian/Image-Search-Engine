bc5f4c562a165bb160c51771a59da7745fbcb6fa
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-21 20:10:00 +0530

    comments remove

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const searchResult = document.getElementById('search-result')
 const showMoreBtn = document.getElementById('show-more-btn')
 
 const key = 'aMdOIZ4C_Nhe5YA6HDCwl4NPj_-rRohjQW_1irwMLGI'
-// aMdOIZ4C_Nhe5YA6HDCwl4NPj_-rRohjQW_1irwMLGI
-// https://api.unsplash.com/search/photos?page=1&query=office
 
 let keyword = ''
 let page = 1
